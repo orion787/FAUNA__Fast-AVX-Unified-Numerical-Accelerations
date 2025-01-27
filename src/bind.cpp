c0ccecc1503a1aa426de43e5e5b1bd89901aa764
orion787 <116973576+orion787@users.noreply.github.com>
2025-01-27 22:04:49 +0300

    Update bind.cpp

--- src/bind.cpp
+++ src/bind.cpp
@@ -1,6 +1,9 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include "avx2_operations.hpp"
+#include "avx_filter_normalize.cpp"
+#include "avx2_convolution.cpp"
+#include "avx2_image_processor.cpp"
 
 namespace py = pybind11;
 
@@ -15,5 +18,14 @@ PYBIND11_MODULE(fauna, m) {
         .def_static("cross_product", &AVX2Operations::cross_product)
         .def_static("vector_matrix_multiply", &AVX2Operations::vector_matrix_multiply)
         .def_static("add_4", &AVX2Operations::add_4);
-}
+    
+    py::class_<AVXFilterNormalize>(m, "AVXFilterNormalize")
+        .def_static("filter_and_normalize", &AVXFilterNormalize::filter_and_normalize,
+                    py::arg("input_array"), py::arg("min_value"), py::arg("max_value"));
 
+    py::class_<AVXConvolution>(m, "AVXConvolution")
+        .def_static("convolve", &AVXConvolution::convolve);
+
+    py::class_<AVXImageProcessor>(m, "AVXImageProcessor")
+        .def_static("compute_average_color", &AVXImageProcessor::compute_average_color);
+}
